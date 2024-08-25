8794e65a85f4d16cf0deff721284576bdd39740d
gustav <gustaavv.git@yahoo.com>
2024-08-25 16:31:28 +0800

    basic layout

---
+++ SCIDesktop/control/DecryptionControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SCIDesktop.control;
+
+public partial class DecryptionControl : UserControl
+{
+    public DecryptionControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SCIDesktop/control/EncryptionControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SCIDesktop.control;
+
+public partial class EncryptionControl : UserControl
+{
+    public EncryptionControl()
+    {
+        InitializeComponent();
+    }
+}
