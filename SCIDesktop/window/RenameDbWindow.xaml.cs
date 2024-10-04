ee2a017a0f350524120fac60acc5a61a8a7ba4b3
gustav <gustaavv.git@yahoo.com>
2024-10-04 12:56:52 +0800

    update RenameDbWindow

--- SCIDesktop/window/RenameDbWindow.xaml.cs
+++ SCIDesktop/window/RenameDbWindow.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using MahApps.Metro.Controls;
 using SCICore.dao;
-using SCICore.entity;
 
 namespace SCIDesktop.window;
 
